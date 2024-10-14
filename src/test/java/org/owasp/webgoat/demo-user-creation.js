1c51188c13ac95c0ddfae81c7034ab53e24f9a53
yanghugo <yanghugo@synopsys.com>
2024-10-14 16:04:17 +0800

    20241014-setupSUT

---
+++ src/test/java/org/owasp/webgoat/demo-user-creation.js
@@ -0,0 +1,3 @@
+var sutUserName = "this-is-a-fake-user";
+var sutUserPassword = "not-really-a-password-this-is-fake";
+var sutAPIToken = "bdh97thhdewbIuytvfm90JytgMR453kri09wrnfryswbnhgqwertyuioDFGHJKCVBNMertyuidfghjkUJMIK"
